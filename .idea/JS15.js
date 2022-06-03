78ca86c9924917a9792f79072219692b32c69465
주동호 <100278301+JOODON@users.noreply.github.com>
2022-06-03 09:00:22 +0900

    Swict

--- .idea/JS15.js
+++ .idea/JS15.js
@@ -1,4 +1,17 @@
-//Symbol
-//실볼은 유일성 보장
-const  id=Symbol("id");
-const  id=Symbol("id");
+let fruit=prompt(`무슨 과일을 사고 싶으 신가요?`);
+//if else랑 비슷함
+switch (fruit){
+    case "사과":
+        console.log("100원");
+    case "파인애플":
+        console.log("200원");
+    case "포도":
+        console.log("300원");
+    case "키위":
+        console.log("400원");
+    case "멜론":
+        console.log("500원");
+        break
+    default:
+        console.log("그런 과일 없수다");
+}
